f0b8564325c394225f266801d811576d03c488e1
Johannes Li <55927198+jli265@users.noreply.github.com>
2020-01-31 18:22:29 -0500

    Update check_prime_numbers.py

--- check_prime_numbers.py
+++ check_prime_numbers.py
@@ -1,25 +1,28 @@
-# find out prime numbers between 1 and the number you entered.
+# find out prime numbers between 1 and the number you enter.
 import math
-
-N=int(input("Enter the biggest positive integer(N>1) that you would test:"))
-
-
+try:
+    N=int(input("Enter the biggest positive integer(N>1) that you would test:"))
+except:
+    print("Error! Please enter an integer greater than 1! Try again!")
+    exit()
 is_prime=True
 prime_list=[]
 prime_list.append(2)
 prime_list.append(3)
-if N==2:
+
+if N <=1:
+    print("Error! Please enter an integer greater than 1! Try again!")
+elif N==2:
    print("Prime number is 2.")
-   print("There is 1 prime number.")
+   print("There is 1 prime number between 1 and 2.")
 elif N==3:
    print("Prime numbers are 2, 3.")
-   print("There are 2 prime numbers.")
+   print("There are 2 prime numbers between 1 and 3.")
 else:
   m=4
   count = 2
   for n in range(m,N+1):
     if n % 2 == 0 :
-        n+=1
         continue
     for i in range(2,math.ceil(math.sqrt(n))+1):
         if n % i ==0:
@@ -28,16 +31,11 @@ else:
     if is_prime==True:
             count+=1
             prime_list.append(n)
-
-
     else:
         is_prime=True
-        n+=1
-
-
-print("Prime numbers are shown below in a list.")
-print(prime_list)
-print("There are %d prime numbers."%(count))
+  print("Prime numbers are shown below in a list.")
+  print(prime_list)
+  print("There are %d prime numbers between 1 and %d."%(count,N))
 
 
 
